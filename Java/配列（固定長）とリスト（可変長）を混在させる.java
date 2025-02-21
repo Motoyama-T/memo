a237a083ea12eefbf13bc8814779d25d91e058f3
Motoyama-T <60880548+Motoyama-T@users.noreply.github.com>
2025-02-21 16:14:51 +0900

    Create 配列（固定長）とリスト（可変長）を混在させる.java

---
+++ Java/配列（固定長）とリスト（可変長）を混在させる.java
@@ -0,0 +1,16 @@
+//一次元目が「配列（固定長）」二次元目が「リスト（可変長）」の二次元構造gの書き方
+import java.util.*;
+
+class Sample {
+  public static void main(String[] args) {
+    int N = 5;  //配列（固定長）の長さ：5
+    ArrayList<Integer>[] g = new ArrayList[N];  //出力：[null,null,null,null,null]
+    for(int i = 0; i < g.length; i ++){
+      g[i] = new ArrayList<Integer>();  //出力：[[],[],[],[],[]]
+    }
+    g[0].add(1);  //出力：[[1],[],[],[],[]]
+    g[0].add(1);  //出力：[[1,1],[],[],[],[]]
+    g[1].add(1);  //出力：[[1,1],[1],[],[],[]]
+    System.out.println(Arrays.toString(g));
+  }
+}
