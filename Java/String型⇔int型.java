f8e6cf01e91bdaa3b02cd0913f00e21e5bebe63a
Motoyama-T <60880548+Motoyama-T@users.noreply.github.com>
2025-03-03 11:52:15 +0900

    Update and rename String型⇔int型.txt to String型⇔int型.java

--- Java/String型⇔int型.java
+++ Java/String型⇔int型.java
@@ -1,10 +1,10 @@
-■String→int
+//■String→int
 public static int change_int(String x){
 	int int_x = Integer.parseInt(x);
         return int_x;
 }
 
-■int→String
+//■int→String
 public static String change_String(int x){
 	String String_x = String.valueOf(x);
         return String_x;
