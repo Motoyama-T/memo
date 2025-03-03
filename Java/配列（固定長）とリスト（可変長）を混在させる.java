87fba9609b18880125659bb839d6a530b7350e1a
Motoyama-T <60880548+Motoyama-T@users.noreply.github.com>
2025-03-03 13:32:22 +0900

    Update 配列（固定長）とリスト（可変長）を混在させる.java

--- Java/配列（固定長）とリスト（可変長）を混在させる.java
+++ Java/配列（固定長）とリスト（可変長）を混在させる.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 class Sample {
   public static void main(String[] args) {
-    int N = 5;  //配列（固定長）の長さ：5
+    int N = 5;                                  //配列（固定長）の長さ：5
     ArrayList<Integer>[] g = new ArrayList[N];  //出力：[null,null,null,null,null]
     for(int i = 0; i < g.length; i ++){
-      g[i] = new ArrayList<Integer>();  //出力：[[],[],[],[],[]]
+      g[i] = new ArrayList<Integer>();          //出力：[[],[],[],[],[]]
     }
-    g[0].add(1);  //出力：[[1],[],[],[],[]]
-    g[0].add(1);  //出力：[[1,1],[],[],[],[]]
-    g[1].add(1);  //出力：[[1,1],[1],[],[],[]]
+    g[0].add(1);                                //出力：[[1],[],[],[],[]]
+    g[0].add(1);                                //出力：[[1,1],[],[],[],[]]
+    g[1].add(1);                                //出力：[[1,1],[1],[],[],[]]
     System.out.println(Arrays.toString(g));
   }
 }
